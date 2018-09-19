7db7b1d2ef0e650adc128ba74b7fd54881a0ef0a
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-09-19 16:44:17 +0300

    Task1.2

---
+++ Task1_2.hs
@@ -0,0 +1,49 @@
+module Task1_2 where
+
+import Todo(todo)
+
+-- синус числа (формула Тейлора)
+sin :: Double -> Double
+sin x = todo
+
+-- косинус числа (формула Тейлора)
+cos :: Double -> Double
+cos x = todo
+
+-- наибольший общий делитель двух чисел
+gcd :: Integer -> Integer -> Integer
+gcd x y = todo
+
+-- существует ли полный целочисленный квадрат в диапазоне [from, to)?
+doesSquareBetweenExist :: Integer -> Integer -> Bool
+doesSquareBetweenExist from to = todo
+
+-- является ли дата корректной с учётом количества дней в месяце и
+-- вискокосных годов?
+isDateCorrect :: Integer -> Integer -> Integer -> Bool
+isDateCorrect day month year = todo
+
+-- возведение числа в степень, duh
+-- готовые функции и плавающую арифметику использовать нельзя
+pow :: Integer -> Integer -> Integer
+pow x y = todo
+
+-- является ли данное число простым?
+isPrime :: Integer -> Bool
+isPrime x = todo
+
+type Point2D = (Double, Double)
+
+-- рассчитайте площадь многоугольника по формуле Гаусса
+-- многоугольник задан списком координат
+shapeArea :: [Point2D] -> Double
+shapeArea points = todo
+
+-- треугольник задан своими координатами.
+-- функция должна вернуть 
+--  0, если он тупоугольный
+--  1, если он остроугольный
+--  2, если он прямоугольный
+--  -1, если это не треугольник
+triangleKind :: Point2D -> Point2D -> Point2D -> Integer
+triangleKind a b c = todo
