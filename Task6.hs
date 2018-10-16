0c742756634ce72affc0076cd53e1f37c16dc33e
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2018-10-16 15:28:16 +0300

    Task 6

---
+++ Task6.hs
@@ -0,0 +1,12 @@
+module Task6 where
+
+import Todo(todo)
+
+data LinkedTree a = ChangeMe
+
+find :: LinkedTree a -> a -> Bool
+find = todo
+insert :: LinkedTree a -> a -> LinkedTree a
+insert = todo
+remove :: LinkedTree a -> a -> LinkedTree a
+remove = todo
